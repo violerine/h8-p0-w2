c8ff70079ff63ca76cec63f19719b6a838e58595
violerine <viosefferine@gmail.com>
2018-02-26 14:41:56 +0700

    third commit

--- exercise-4.js
+++ exercise-4.js
@@ -1,30 +1,33 @@
 var tanggal = 1
 var bulan =7
 var tahun =1994
+var hasil =""
+
 
 switch(bulan){
     case 1: 
-    console.log(tanggal + " Januari " + tahun);break
+    date = tanggal + " Januari " + tahun ;break
     case 2: 
-    console.log(tanggal + " Februari " + tahun);break
+    date= tanggal + " Februari " + tahun;break
     case 3: 
-    console.log(tanggal + " Maret " + tahun);break
+    date = tanggal + " Maret " + tahun ;break
     case 4: 
-    console.log(tanggal + " April " + tahun);break
+    date = tanggal + " April " + tahun ;break
     case 5: 
-    console.log(tanggal + " Mei " + tahun);break
+    date = tanggal + " Mei " + tahun ;break
     case 6: 
-    console.log(tanggal + " Juni " + tahun);break
+    date = tanggal + " Juni " + tahun ;break
     case 7: 
-    console.log(tanggal + " July " + tahun);break
+    date = tanggal + " July " + tahun ;break
     case 8: 
-    console.log(tanggal + " Agustus " + tahun);break
+    date = tanggal + " Agustus " + tahun;break
     case 9: 
-    console.log(tanggal + " September " + tahun);break
+    date = tanggal + " September " + tahun ;break
     case 10: 
-    console.log(tanggal + " Oktober " + tahun);break
+    date = tanggal + " Oktober " + tahun ;break
     case 11: 
-    console.log(tanggal + " November " + tahun);break
+    date = tanggal + " November " + tahun; break
     case 12: 
-    console.log(tanggal + " Desember " + tahun);break
+    date = tanggal + " Desember " + tahun ;break
 }
+console.log(date)
